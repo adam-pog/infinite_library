ba85a260ce302f79debf1b1a77e84ac0e5f74549
Adam Pogwizd <adampog77@gmail.com>
2019-07-06 16:21:08 -0500

    formatting

--- encrypt.go
+++ encrypt.go
@@ -1,115 +1,112 @@
 package main
 
 import (
-    "crypto/aes"
-    "crypto/cipher"
-    // "fmt"
-    "time"
-    "strings"
-    "net/http"
-    // "runtime"
-    // "reflect"
-    "io"
-    "github.com/julienschmidt/httprouter"
-    "log"
-    "os"
+	"crypto/aes"
+	"crypto/cipher"
+	// "fmt"
+	"net/http"
+	"strings"
+	"time"
+	// "runtime"
+	// "reflect"
+	"github.com/julienschmidt/httprouter"
+	"io"
+	"log"
+	"os"
 )
 
 func main() {
-    router := httprouter.New()
-    router.GET("/book/:num", book)
+	router := httprouter.New()
+	router.GET("/book/:num", book)
 
-    server := http.Server{
-      Addr: "localhost:8080",
-      Handler: logger(router),
-    }
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: logger(router),
+	}
 
-    server.ListenAndServe()
+	server.ListenAndServe()
 }
 
 func logger(router http.Handler) http.Handler {
-    logger := log.New(os.Stdout, "http: ", log.LstdFlags)
-    logger.Println("Serving http://localhost:8080")
+	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
+	logger.Println("Serving http://localhost:8080")
 
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        start := time.Now()
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
 
-        router.ServeHTTP(w, r)
+		router.ServeHTTP(w, r)
 
-        // log request by who(IP address)
-        requesterIP := r.RemoteAddr
-
-        logger.Printf("%s\t\t%s", r.Method, r.RequestURI)
-        log.Printf(
-                "Sent\t\t%s\t\t%v\n\n",
-                requesterIP,
-                time.Since(start),
-        )
-    })
+		logger.Printf("%s\t\t%s", r.Method, r.RequestURI)
+		log.Printf(
+			"Sent\t\t%s\t\t%v\n\n",
+			r.RemoteAddr,
+			time.Since(start),
+		)
+	})
 }
 
-func book(w http.ResponseWriter, r *http.Request, p httprouter.Params){
-  num_arr := strings.Split(p.ByName("num"), "")
-  plaintext := bytify(num_arr)
-  cipher_text := encrypt(key, iv, plaintext)
+func book(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	num_arr := strings.Split(p.ByName("num"), "")
+	plaintext := bytify(num_arr)
+	cipher_text := encrypt(key, iv, plaintext)
 
-  io.WriteString(w, readable(cipher_text[0:3199]))
-  io.WriteString(w, "\n\n\n")
-  io.WriteString(w, readable(cipher_text[1308800:1312000]))
+	io.WriteString(w, readable(cipher_text[0:3199]))
+	io.WriteString(w, "\n\n\n")
+	io.WriteString(w, readable(cipher_text[1308800:1312000]))
 }
 
 func encrypt(key []byte, iv []byte, plaintext []byte) []byte {
-    block, _ := aes.NewCipher(key);
+	block, _ := aes.NewCipher(key)
 
-    mode := cipher.NewCBCEncrypter(block, iv);
-    enc := make([]byte, Size)
-    mode.CryptBlocks(enc, plaintext);
+	mode := cipher.NewCBCEncrypter(block, iv)
+	enc := make([]byte, Size)
+	mode.CryptBlocks(enc, plaintext)
 
-    mode = cipher.NewCBCEncrypter(block, iv);
-    final := make([]byte, Size)
-    mode.CryptBlocks(final, reverse(enc));
-    return final
+	mode = cipher.NewCBCEncrypter(block, iv)
+	final := make([]byte, Size)
+	mode.CryptBlocks(final, reverse(enc))
+	return final
 }
 
 func decrypt(key []byte, iv []byte, encryptedText []byte) []byte {
-    block, _ := aes.NewCipher(key);
+	block, _ := aes.NewCipher(key)
 
-    mode := cipher.NewCBCDecrypter(block, iv);
-    dec := make([]byte, Size)
-    mode.CryptBlocks(dec, encryptedText);
+	mode := cipher.NewCBCDecrypter(block, iv)
+	dec := make([]byte, Size)
+	mode.CryptBlocks(dec, encryptedText)
 
-    mode = cipher.NewCBCDecrypter(block, iv);
-    final := make([]byte, Size)
-    mode.CryptBlocks(final, reverse(dec));
+	mode = cipher.NewCBCDecrypter(block, iv)
+	final := make([]byte, Size)
+	mode.CryptBlocks(final, reverse(dec))
 
-    return final
+	return final
 }
 
 func readable(text []byte) string {
-    plaintext := make([]string, 3200);
-    for i, v := range text {
-        plaintext[i] = NumToCharMap[v]
-    }
+	plaintext := make([]string, 3200)
+	for i, v := range text {
+		plaintext[i] = NumToCharMap[v]
+	}
 
-    // fmt.Println(strings.Join(plaintext, ""))
-    return strings.Join(plaintext, "")
+	// fmt.Println(strings.Join(plaintext, ""))
+	return strings.Join(plaintext, "")
 }
 
 func reverse(arr []byte) []byte {
-    rev := make([]byte, len(arr))
+	rev := make([]byte, len(arr))
 
-    for i, j := len(arr)-1, 0; i >= 0; i, j = i-1, j+1 {
-       rev[j] = arr[i]
-    }
+	for i, j := len(arr)-1, 0; i >= 0; i, j = i-1, j+1 {
+		rev[j] = arr[i]
+	}
 
-    return rev
+	return rev
 }
 
-func bytify(textArr []string) []byte{
-    plaintextBytes := make([]byte, Size);
-    for i, v := range textArr {
-        plaintextBytes[i] = CharToNumMap[v]
-    }
+func bytify(textArr []string) []byte {
+	plaintextBytes := make([]byte, Size)
+	for i, v := range textArr {
+		plaintextBytes[i] = CharToNumMap[v]
+	}
 
-    return plaintextBytes
+	return plaintextBytes
 }
