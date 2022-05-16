63fe09b4356e8eaf5a779e7d4bf62a5e346a8817
Adam Pogwizd <adampog77@gmail.com>
2022-05-15 19:46:55 -0600

    accept image for book lookup

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
 from Crypto.Random import get_random_bytes
 from mappings import NUM_TO_CHAR_MAP
+from PIL import Image
+from base64 import b64decode
+from io import BytesIO
 
 app = Flask(__name__)
 CORS(app)
@@ -15,19 +18,38 @@ BOOK_PAD = 1312000
 
 @app.route('/book', methods=['POST'])
 def get_page():
+    body = request.json
+
     return {
         "text": split_text(
             generate_text(
-                str(request.json['book']),
-                int(request.json['page'])
+                create_plaintext_book(body['book']),
+                int(body['page'])
             )
         )
     }
 
 
+# def test(arr):
+#     cipher = AES.new(KEY, AES.MODE_CBC, iv=IV)
+#     pass1 = cipher.encrypt(arr)
+#     print(list(pass1))
+#     cipher = AES.new(KEY, AES.MODE_CBC, iv=IV)
+#     pass2 = cipher.encrypt(pass1[::-1])
+#     print(list(pass2))
+#     dcipher = AES.new(KEY, AES.MODE_CBC, iv=IV)
+#     decrypt1 = dcipher.decrypt(pass2)
+#     print(list(decrypt1[::-1]))
+#     dcipher = AES.new(KEY, AES.MODE_CBC, iv=IV)
+#     decrypt2 = dcipher.decrypt(decrypt1[::-1])
+#     return decrypt2
+
+def pad(book):
+    return book + ([0] * (BOOK_PAD - len(book)))
+
 def generate_text(book, page):
     cipher = AES.new(KEY, AES.MODE_CBC, iv=IV)
-    pass1 = cipher.encrypt(str(book).encode().ljust(BOOK_PAD))
+    pass1 = cipher.encrypt(bytearray(pad(book)))
     cipher = AES.new(KEY, AES.MODE_CBC, iv=IV)
     pass2 = cipher.encrypt(pass1[::-1])
 
@@ -44,8 +66,16 @@ def split_text(text):
         text[(i * 80):(i * 80) + 80] for i in range(40)
     ]
 
+def create_plaintext_book(img):
+    _, data = request.json['book'].split(',', 1)
+    img = Image.open(BytesIO(b64decode(data)))
+    pixels = img.getdata()
 
+    book = []
+    for pixel_tuple in pixels:
+        book += [p for p in pixel_tuple]
 
+    return book
 
 
 # dcipher = AES.new(KEY, AES.MODE_CBC, iv=IV)
